1c09c79b0d46105c724203336e67c541bb55daaf
Kenny Adetiloye <kehinde.adetiloye@gmail.com>
2022-11-08 09:52:16 -0500

    Revamp code to fix global date issue

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -1,9 +1,5 @@
 export const dateFormatter = (date: Date) => {
-  const days = date?.toLocaleDateString().split('.');
-  let newDate = '';
-  if (days) {
-    newDate = `${days[0]}`;
-  }
+  const newDate = date?.toLocaleDateString('en-029');
 
   return newDate;
 };
