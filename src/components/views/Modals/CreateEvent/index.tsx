d68f552ab1436198ebe1a2f47cd83152631465eb
Kenny Adetiloye <kehinde.adetiloye@gmail.com>
2022-11-14 12:26:40 -0500

    Add cancel button and changed address to location and phone to contact number

--- src/components/views/Modals/CreateEvent/index.tsx
+++ src/components/views/Modals/CreateEvent/index.tsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { ModalContext } from 'context/Modal';
+
 import {
   TextArea,
   EventFile,
@@ -12,45 +16,49 @@ import { stateOptions, peopleOptions } from './options';
 
 import styles from './CreateEvent.module.scss';
 
-const CreateEvent = () => (
-  <div className={styles.container}>
-    <div className={styles.container__content}>
-      <div className={styles.container__content_head}>CREATE EVENT</div>
-      <div className={styles.container__content_main}>
-        <div className={styles.container__content_main_left}>
-          <EventInput title='Event Name' />
-          <div className={styles.container__content_main_left_item}>
-            <DatePickerSelect title='Date' />
-            <HourPicker />
+const CreateEvent = () => {
+  const { closeModal } = useContext(ModalContext);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__content}>
+        <div className={styles.container__content_head}>CREATE EVENT</div>
+        <div className={styles.container__content_main}>
+          <div className={styles.container__content_main_left}>
+            <EventInput title='Event Name' />
+            <div className={styles.container__content_main_left_item}>
+              <DatePickerSelect title='Date' />
+              <HourPicker />
+            </div>
+            <div className={styles.container__content_main_left_item}>
+              <CreateEventDropDown title='State' size='small' optionList={stateOptions} />
+              <EventInput title='City' small />
+            </div>
+            <div className={styles.container__content_main_left_item}>
+              <EventInput title='Location' small />
+              <EventInput title='Contact Number' small />
+            </div>
           </div>
-          <div className={styles.container__content_main_left_item}>
-            <CreateEventDropDown title='State' size='small' optionList={stateOptions} />
-            <EventInput title='City' small />
-          </div>
-          <div className={styles.container__content_main_left_item}>
-            <EventInput title='Address' small />
-            <EventInput title='Number' small />
+          <div className={styles.container__content_main_right}>
+            <div className={styles.container__content_main_right_item}>
+              <TextArea />
+            </div>
+            <div className={styles.container__content_main_right_item}>
+              <CreateEventDropDown title='People' size='small' optionList={peopleOptions} />
+              <EventPrice />
+            </div>
+            <div className={styles.container__content_main_right_elem}>
+              <EventFile />
+            </div>
           </div>
         </div>
-        <div className={styles.container__content_main_right}>
-          <div className={styles.container__content_main_right_item}>
-            <TextArea />
-          </div>
-          <div className={styles.container__content_main_right_item}>
-            <CreateEventDropDown title='People' size='small' optionList={peopleOptions} />
-            <EventPrice />
-          </div>
-          <div className={styles.container__content_main_right_elem}>
-            <EventFile />
-          </div>
+        <div className={styles.container__content_footer}>
+          <button onClick={closeModal}>Cancel</button>
+          <button>Create</button>
         </div>
-      </div>
-      <div className={styles.container__content_footer}>
-        <button>Clean</button>
-        <button>Create</button>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CreateEvent;
